983497df8f71a9641acba99c7dd7af434a81da00
Piyush <piyush.hole2001@gmail.com>
2024-10-01 17:58:30 +0530

    chngs

--- pkl.py
+++ pkl.py
@@ -1,9 +1,11 @@
-import tensorflow as tf
 import pickle
+from keras.models import load_model
 
-# Load the .h5 model
-model = tf.keras.models.load_model('model/model_sev.h5')
+# Step 1: Load the Keras model from H5 file
+model = load_model('model/model_sev.h5')  # Replace 'your_model.h5' with the path to your H5 file
 
-# Convert the model to a pickle file
-with open('model_sev.pkl', 'wb') as f:
-    pickle.dump(model, f)
+# Step 2: Save the model to a pickle file
+with open('model.pkl', 'wb') as file:
+    pickle.dump(model, file)
+
+print("Model successfully converted from H5 to PKL format.")
